3540c70b2588978bf7de2a5a97a25a3ad621c191
Jacob Brower <jacobbrower@jacobs-mbp-2.wireless.rit.edu>
2019-02-02 01:08:21 -0500

    Static Dim White Color

--- Music_Pulse/Music_Pulse.ino
+++ Music_Pulse/Music_Pulse.ino
@@ -1,27 +1,25 @@
+//The FastLED Library
 #include <FastLED.h>
 
+//My Personal number of led pixels and the associated led pin on the
+//Arduino board
 #define NUM_LEDS  150
 #define LED_PIN   7
 
+//Setting up the string of leds
 CRGB leds[NUM_LEDS];
 
+int ledsMidPoint = NUM_LEDS / 2;
+
 void setup() {
+  //Adding the led strand so it is recognisable to the FastLed library
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(20, 20, 20);
+  }
+  FastLED.show();
 }
 
-int i = 0;
 void loop() {
-  leds[i] = CRGB(255, 0, 0);
-  leds[i+1] = CRGB(255, 0, 0);
-  leds[i+2] = CRGB(255, 0, 0);
-  leds[i+3] = CRGB(255, 0, 0);
-  leds[i+4] = CRGB(255, 0, 0);
-  leds[i-1] = CRGB(0, 0, 0);
-  FastLED.show();
-  FastLED.delay(30);
-  if (i > NUM_LEDS) {
-    i = 0;
-  } else {
-    i++;
-  }
+  
 }
