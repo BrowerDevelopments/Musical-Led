0f065ba38effe2cba74e0b9c376d0840b590646a
Jacob Brower <jacobbrower@jacobs-mbp-2.wireless.rit.edu>
2019-02-02 00:25:53 -0500

    The LED setup

--- Music_Pulse/Music_Pulse.ino
+++ Music_Pulse/Music_Pulse.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <FastLED.h>
 
+#define NUM_LEDS  150
+#define LED_PIN   7
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
 }
 
+int i = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  leds[i] = CRGB(255, 0, 0);
+  leds[i+1] = CRGB(255, 0, 0);
+  leds[i+2] = CRGB(255, 0, 0);
+  leds[i+3] = CRGB(255, 0, 0);
+  leds[i+4] = CRGB(255, 0, 0);
+  leds[i-1] = CRGB(0, 0, 0);
+  FastLED.show();
+  FastLED.delay(30);
+  if (i > NUM_LEDS) {
+    i = 0;
+  } else {
+    i++;
+  }
 }
